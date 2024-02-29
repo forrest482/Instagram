d2d06a1c1ca940c061a3b702d47203954700c776
Mohammad Ali <forrest482@outlook.com>
2024-02-29 08:39:44 +0330

    add apps

---
+++ instagram/local_settings.py
@@ -0,0 +1,10 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-4(+-12bjim%)=pnl(7q&wfme#=44@=a28ub@^oh4h^$4exk2ij'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+ADMIN_PATH = 'admin/'
